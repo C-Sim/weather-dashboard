ec62d48ef7792e10345aa4d058dcbafcfcead7f5
CSim <100964977+C-Sim@users.noreply.github.com>
2022-05-09 18:20:22 +0100

    API framework

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,10 @@ const searchHistory = $("#search-history");
 
 const cityButton = $("#city-button");
 
+const weatherContainer = $("#weatherContainer");
+
+const currentDate = moment().format("dddd Do MMMM YYYY");
+
 // initialise LS
 const onReady = () => {
   // check if cities exists in LS
@@ -47,7 +51,7 @@ const renderCities = () => {
     searchHistoryContainer.append(`<div id="search-history">
     <div>`);
 
-    for (let i = 0; i < 5; i += 1) {
+    for (let i = 0; i < 6; i += 1) {
       if (!recentCities[i]) {
       } else {
         searchHistoryContainer.append(`<button name="city-button" id="city-button" type="submit">${recentCities[i]}</button></div>
@@ -100,7 +104,7 @@ const handleButtonClick = (event) => {
 
     writeToLocalStorage("cities", recentCities);
 
-    renderWeatherData();
+    renderWeatherData(city);
   }
 };
 
@@ -116,39 +120,109 @@ const renderWeatherData = (city) => {
   const fetchOptions = { method: "GET" };
 
   // use API to fetch current weather data
-  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  //   TODO - add ,uk after city to specify??
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-  const handleAsync = async () => {
+  const handleCurrentAsync = async () => {
     try {
+      // make request to API
       const response = await fetch(currentWeatherUrl, fetchOptions);
+      //   check response status
       if (response.ok) {
-        const data = await response.json();
+        //   return data
+        const currentData = await response.json();
+        return currentData;
       } else {
-        throw new error("error");
+        //   return error
+        throw new error("Couldn't fetch data");
       }
     } catch (error) {
-      handleError();
+      // respond to error
+      throw new Error(error.message);
     }
   };
 
   // from the response cherry pick all the data you want to see in the current weather card
+  const currentWeatherData = (currentData) => {
+    const cityName = currentData?.name || [];
+    const cityLatitude = currentData?.coord?.lat || [];
+    const cityLongitude = currentData?.coord?.long || [];
+    //   const avTemp = currentData?.main?.temp || [];
+    //   const humidity = currentData?.main?.humidity || [];
+    //   const windSpeed = currentData?.wind?.speed*2.23694 || [];
+    //   const weatherIcon = currentData?.weather?.icon || [];
+  };
 
   // get the lat and lon from current weather data API response
-  const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
+  const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}&units=metric`;
+
+  const handleOneCallAsync = async () => {
+    try {
+      // make request to API
+      const response = await fetch(forecastWeatherUrl, fetchOptions);
+      //   check response status
+      if (response.ok) {
+        //   return data
+        const forecastData = await response.json();
+        return forecastData;
+      } else {
+        //   return error
+        throw new error("Couldn't fetch data");
+      }
+    } catch (error) {
+      // respond to error
+      throw new Error(error.message);
+    }
+  };
+
+  const forecastWeatherData = (forecastData) => {
+    const Latitude = forecastData?.lat || [];
+    const Longitude = forecastData?.long || [];
+    const avTemp = forecastData?.current?.temp || [];
+    const humidity = forecastData?.current?.humidity || [];
+    const windSpeed = forecastData?.current?.wind_speed * 2.23694 || [];
+    const weatherIcon = forecastData?.current?.weather?.icon || [];
+    const date = forecastData?.current?.dt || [];
+    const uvIndex = forecastData?.current?.uvi || [];
+  };
 
   // render current weather data
-  renderCurrentWeather();
+  renderCurrentWeather(currentWeatherData);
 
   // render forecast weather data
-  renderForecastWeather();
+  renderForecastWeather(forecastWeatherData);
 };
 
 const renderCurrentWeather = (currentWeatherData) => {
   // render the current weather data and append to section
+  weatherContainer.append(`<div class="current-weather">
+<img class="weather-icon" src=${weatherIcon} />
+<div class="current-weather-info">
+  <h1>${cityName}</h1>
+  <h2>${currentDate}</h2>
+  <p>Temperature: <span>${avTemp}</span></p>
+  <p>Wind: <span>${windSpeed}</span></p>
+  <p>Humidity: <span>${humidity}</span></p>
+  <p id="indexUV">UV index: <span class="highUV">${uvIndex}</span></p>
+</div>
+</div>`);
 };
 
 const renderForecastWeather = (forecastWeatherData) => {
   // render the forecast weather data and append each card to section
+  //   for (let i = currentDate ++ 1, i < currentDate ++ 5, i +=1) {
+  weatherContainer.append(`<div id="forecast-container">
+  <div class="forecast">
+    <div>
+      <img class="small-weather-icon" src=${weatherIcon} />
+    </div>
+    <div class="forecast-weather-info">
+      <h2>${[i]}</h2>
+      <p>Temperature: <span>${avTemp}</span></p>
+      <p>Wind: <span>${windSpeed}</span></p>
+      <p>Humidity: <span>${humidity}</span></p>
+    </div>
+  </div>`);
 };
 
 $(searchHistoryContainer).click(handleButtonClick);
