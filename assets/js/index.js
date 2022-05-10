6802a3466fceef0d25405027f26774701aff380e
CSim <100964977+C-Sim@users.noreply.github.com>
2022-05-10 19:38:16 +0100

    experiment with rendering weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,9 @@ const cityButton = $("#city-button");
 
 const weatherContainer = $("#weatherContainer");
 
-const currentDate = moment().format("dddd Do MMMM YYYY");
+const forecastContainer = $("#forecast-container");
+
+const currentDate = moment.unix().format("dddd Do MMMM YYYY");
 
 // initialise LS
 const onReady = () => {
@@ -37,44 +39,47 @@ const readFromLocalStorage = () => {
   return parsedData;
 };
 
+// TODO x2
 const renderCities = () => {
   // get recent cities from LS []
   const recentCities = readFromLocalStorage();
 
-  const message = "Your recent searches will be saved here.";
+  const message = "Your 6 most recent searches will be saved here.";
 
   // if [] is empty then render alert
-  if (recentCities.length === 0) {
+  if (!recentCities.length) {
     searchHistoryContainer.append(`<h2 class="message">${message}</h2>`);
   } else {
     // else render all recent cities
     searchHistoryContainer.append(`<div id="search-history">
     <div>`);
 
+    // TODO try map function
     for (let i = 0; i < 6; i += 1) {
       if (!recentCities[i]) {
       } else {
-        searchHistoryContainer.append(`<button name="city-button" id="city-button" type="submit">${recentCities[i]}</button></div>
+        searchHistoryContainer.append(`<button data-city="${recentCities[i]}" id="city-button" type="submit">${recentCities[i]}</button></div>
     <div>`);
       }
     }
   }
-
-  // add an event listener on div containing all cities
 };
 
-const handleFormSubmit = (event) => {
+// TODO remove duplicates in search history
+const handleFormSubmit = async (event) => {
   event.preventDefault();
 
   // get the city name from input
   const city = formInput.val();
 
+  const renderStatus = await renderWeatherData(city);
+
   const message = "Please enter a city.";
   // if city name is empty display message
   if (!city) {
     form.append(`<h2 class="message">${message}</h2>`);
-  } else {
-    // else write to local storage and render weather data
+  } else if (renderStatus) {
+    // else if status is positive, write to local storage and render weather data
 
     // add city to start of array in LS
     const recentCities = readFromLocalStorage();
@@ -82,21 +87,24 @@ const handleFormSubmit = (event) => {
 
     writeToLocalStorage("cities", recentCities);
 
-    renderWeatherData(city);
+    searchHistoryContainer.children().last().remove();
+
+    renderCities();
   }
 };
 
-const handleButtonClick = (event) => {
-  event.preventDefault();
+const handleButtonClick = async (event) => {
+  //   event.preventDefault();
 
   // find target from search history section
   const target = $(event.target);
 
   // if target is button, record value and write to LS
-  if (target.is('button[name="city-button"]')) {
-    // TODO - fix search history click function - returning button text as ""
-    const city = cityButton.text();
-    console.log(city);
+  if (target.is("button")) {
+    // TODO - fix search history click function - returning button text as null
+    const city = target.attr("data-city");
+
+    await renderWeatherData(city);
 
     // add city to start of array in LS
     const recentCities = readFromLocalStorage();
@@ -104,7 +112,9 @@ const handleButtonClick = (event) => {
 
     writeToLocalStorage("cities", recentCities);
 
-    renderWeatherData(city);
+    searchHistoryContainer.children().last().remove();
+
+    renderCities();
   }
 };
 
@@ -115,115 +125,145 @@ const writeToLocalStorage = (key, value) => {
   localStorage.setItem(key, stringifiedValue);
 };
 
-const renderWeatherData = (city) => {
-  // set http method
-  const fetchOptions = { method: "GET" };
+const constructUrl = (baseUrl, params) => {
+  const queryParams = new URLSearchParams(params).toString();
 
-  // use API to fetch current weather data
-  //   TODO - add ,uk after city to specify??
-  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-  const handleCurrentAsync = async () => {
-    try {
-      // make request to API
-      const response = await fetch(currentWeatherUrl, fetchOptions);
-      //   check response status
-      if (response.ok) {
-        //   return data
-        const currentData = await response.json();
-        return currentData;
-      } else {
-        //   return error
-        throw new error("Couldn't fetch data");
-      }
-    } catch (error) {
-      // respond to error
-      throw new Error(error.message);
-    }
-  };
-
-  // from the response cherry pick all the data you want to see in the current weather card
-  const currentWeatherData = (currentData) => {
-    const cityName = currentData?.name || [];
-    const cityLatitude = currentData?.coord?.lat || [];
-    const cityLongitude = currentData?.coord?.long || [];
-    //   const avTemp = currentData?.main?.temp || [];
-    //   const humidity = currentData?.main?.humidity || [];
-    //   const windSpeed = currentData?.wind?.speed*2.23694 || [];
-    //   const weatherIcon = currentData?.weather?.icon || [];
-  };
-
-  // get the lat and lon from current weather data API response
-  const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}&units=metric`;
-
-  const handleOneCallAsync = async () => {
-    try {
-      // make request to API
-      const response = await fetch(forecastWeatherUrl, fetchOptions);
-      //   check response status
-      if (response.ok) {
-        //   return data
-        const forecastData = await response.json();
-        return forecastData;
-      } else {
-        //   return error
-        throw new error("Couldn't fetch data");
-      }
-    } catch (error) {
-      // respond to error
-      throw new Error(error.message);
-    }
-  };
-
-  const forecastWeatherData = (forecastData) => {
-    const Latitude = forecastData?.lat || [];
-    const Longitude = forecastData?.long || [];
-    const avTemp = forecastData?.current?.temp || [];
-    const humidity = forecastData?.current?.humidity || [];
-    const windSpeed = forecastData?.current?.wind_speed * 2.23694 || [];
-    const weatherIcon = forecastData?.current?.weather?.icon || [];
-    const date = forecastData?.current?.dt || [];
-    const uvIndex = forecastData?.current?.uvi || [];
-  };
-
-  // render current weather data
-  renderCurrentWeather(currentWeatherData);
-
-  // render forecast weather data
-  renderForecastWeather(forecastWeatherData);
+  return queryParams ? `${baseUrl}?${queryParams}` : baseUrl;
 };
 
-const renderCurrentWeather = (currentWeatherData) => {
+const fetchData = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const fetchWeatherData = async (city) => {
+  // use API to fetch current weather data
+  console.log(city);
+  const currentWeatherUrl = constructUrl(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      q: city,
+      appid: API_KEY,
+    }
+  );
+
+  // await fetch response
+  const currentData = await fetchData(currentWeatherUrl);
+
+  // get latitude and longitude for city names
+  const cityName = currentData?.name || [];
+  const latitude = currentData?.coord?.lat || [];
+  const longitude = currentData?.coord?.lon || [];
+
+  // apply latitude and longitude to onecall api
+  const forecastWeatherUrl = constructURL(
+    "https://api.openweathermap.org/data/2.5/onecall",
+    {
+      lat: latitude,
+      lon: longitude,
+      exclude: "minutely,hourly",
+      units: "metric",
+      appid: API_KEY,
+    }
+  );
+
+  // await fetch response
+  const forecastData = await fetchData(forecastWeatherUrl);
+
+  // return data retrieved from api
+  return {
+    name: cityName,
+    weatherData: forecastData,
+  };
+};
+
+const renderWeatherData = async (city) => {
+  try {
+    // fetch weather data
+    const weatherData = await fetchWeatherData(city);
+
+    // empty container
+    weatherInfoContainer.empty();
+
+    // render current data
+    renderCurrentWeather(weatherData);
+
+    // render forecast data
+    renderForecastWeather(weatherData);
+
+    return true;
+  } catch (error) {
+    renderError();
+    return false;
+  }
+};
+
+const renderCurrentWeather = (data) => {
   // render the current weather data and append to section
   weatherContainer.append(`<div class="current-weather">
-<img class="weather-icon" src=${weatherIcon} />
-<div class="current-weather-info">
-  <h1>${cityName}</h1>
-  <h2>${currentDate}</h2>
-  <p>Temperature: <span>${avTemp}</span></p>
-  <p>Wind: <span>${windSpeed}</span></p>
-  <p>Humidity: <span>${humidity}</span></p>
-  <p id="indexUV">UV index: <span class="highUV">${uvIndex}</span></p>
-</div>
-</div>`);
+    <img class="weather-icon" src=${weatherIcon} />
+    <div class="current-weather-info">
+  <h1>${data.name}</h1>
+  <h2>${moment
+    .unix(data.weatherData.current.dt)
+    .format("dddd Do MMMM YYYY")}</h2>
+  <p>Temperature: <span>${data.weatherData.current.temp}&deg;C</span></p>
+  <p>Wind: <span>${data.weatherData.current.wind_speed * 2.23694}</span></p>
+  <p>Humidity: <span>${data.weatherData.current.humidity}</span></p>
+  <p id="indexUV">UV index: <span class="highUV">${
+    data.weatherData.current.uvi
+  }</span></p>
+    </div>
+    </div>`);
 };
 
-const renderForecastWeather = (forecastWeatherData) => {
+const renderForecastWeather = (data) => {
   // render the forecast weather data and append each card to section
-  //   for (let i = currentDate ++ 1, i < currentDate ++ 5, i +=1) {
-  weatherContainer.append(`<div id="forecast-container">
-  <div class="forecast">
-    <div>
-      <img class="small-weather-icon" src=${weatherIcon} />
-    </div>
-    <div class="forecast-weather-info">
-      <h2>${[i]}</h2>
-      <p>Temperature: <span>${avTemp}</span></p>
-      <p>Wind: <span>${windSpeed}</span></p>
-      <p>Humidity: <span>${humidity}</span></p>
-    </div>
-  </div>`);
+  const createForecastByDate = (each) => {
+    const forecast = `<div id="forecast-container">
+    <div class="forecast">
+        <div>
+        <img class="small-weather-icon" src=${each.weather[0].icon} />
+        </div>
+        <div class="forecast-weather-info">
+        <h2>${moment.unix(each.dt).format("Do MMM")}</h2>
+        <p>Temperature: <span>${each.temp.day}</span></p>
+        <p>Wind: <span>${each.wind_speed}</span></p>
+        <p>Humidity: <span>${each.humidity}</span></p>
+        </div>
+    </div>`;
+
+    return forecast;
+  };
+
+  const forecastByDate = data.weatherData.daily
+    .slice(1, 6)
+    .map(createForecastByDate)
+    .join("");
+
+  forecastContainer.append(forecastByDate);
 };
+
+const renderError = () => {
+  //    remove existing data from container
+  weatherContainer.empty();
+
+  const message = "Oops, that didn't work. Please try again.";
+
+  const alert = weatherContainer.append(`<h2 class="message">${message}</h2>`);
+};
+
+const uvIndexColor = () => {};
 
 $(searchHistoryContainer).click(handleButtonClick);
 
